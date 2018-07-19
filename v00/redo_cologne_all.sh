97ceb4013275782614e0dcdce0bd4ccf510dd739
James Funderburk <jfunderb@dialog6.rrz.uni-koeln.de>
2018-07-19 05:04:34 +0200

    finish installation of gra, bur, cae, stc, pwg, mw, skd, ae

--- v00/redo_cologne_all.sh
+++ v00/redo_cologne_all.sh
@@ -1,6 +1,8 @@
 python generate.py gra inventory.txt  makotemplates ../../GRAScan/2014/web
 python generate.py bur inventory.txt  makotemplates ../../BURScan/2013/web
-#python generate.py cae inventory.txt  makotemplates ../../cae/web
-#python generate.py stc inventory.txt  makotemplates ../../stc/web
-#python generate.py pwg inventory.txt  makotemplates ../../pwg/web
-#python generate.py mw inventory.txt  makotemplates ../../mw/web
+python generate.py cae inventory.txt  makotemplates ../../CAEScan/2014/web
+python generate.py stc inventory.txt  makotemplates ../../STCScan/2013/web
+python generate.py pwg inventory.txt  makotemplates ../../PWGScan/2013/web
+python generate.py mw inventory.txt  makotemplates ../../MWScan/2014/web
+python generate.py skd inventory.txt  makotemplates ../../SKDScan/2013/web
+python generate.py ae inventory.txt  makotemplates ../../AEScan/2014/web
