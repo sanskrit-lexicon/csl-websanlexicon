2c0685688dc717628f5ba1f14c38e8329afe01e1
James Funderburk <jfunderb@dialog6.rrz.uni-koeln.de>
2018-09-13 23:39:07 +0200

    revise to correct for  (a)Cologne memory error and (b) MW filter condition

--- v00/makotemplates/sqlite/make_input.php
+++ v00/makotemplates/sqlite/make_input.php
@@ -1,14 +1,22 @@
 <?php
- $filein1 = $argv[1]; //"X.xml";
+/* Got memory error (09/13/2018):
+    PHP Fatal error:  Allowed memory size of 134217728 bytes exhausted (tried to allocate 628 bytes) in /afs/
+   Refactor to read lines one at a time
+*/
+ $filein = $argv[1]; //"X.xml";
  $fileout = $argv[2]; //"input.txt";
-
- $lines = file($filein1);
+ $fpin = fopen($filein,"r") or die("Cannot open $filein\n");
+ #$lines = file($filein);  // this is line that failed.
  $fpout = fopen($fileout,"w") or die("Cannot open $fileout\n");
  $lnum=0;
- echo count($lines) . " lines from $filein1\n";
- foreach($lines as $line){
+ #echo count($lines) . " lines from $filein\n";
+ $nlines = 0;
+ while (($line = fgets($fpin)) !== false) {
+ //foreach($lines as $line){
+  $nlines = $nlines + 1;
   $line = trim($line);
-  if (!preg_match('|^<H1>|',$line)) {continue;}
+  if (!preg_match('|^<H|',$line)) {
+   continue;}
   // construct output
   $lnum = $lnum + 1;
   if(!preg_match('|<key1>(.*?)</key1>.*<L>(.*?)</L>|',$line,$matches)) {
@@ -22,6 +30,8 @@
   fwrite($fpout,"$out\n");
  }
  fclose($fpout);
+ fclose($fpin);
+ echo  "$nlines lines from $filein\n";
 exit(0);
 
 ?>
