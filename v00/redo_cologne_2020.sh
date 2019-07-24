981fae5ac856a05430c1510354f0bc6a12e84d56
drdhaval2785 <drdhaval2785@gmail.com>
2019-07-24 16:40:22 +0530

    redo has better logs

--- v00/redo_cologne_2020.sh
+++ v00/redo_cologne_2020.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-echo "Generate the code from template for each dictionary."
-dicts=(ACC AE AP90 AP BEN BHS BOP BOR BUR CAE CCS GRA GST IEG INM KRM MCI MD MW72 MWE MW PD PE PGN PUI PWG PW SCH SHS SKD SNP STC VCP VEI WIL YAT)
+# Regenerate the web display code for specified dictionaries.
+# Usage - bash redo_cologne_2020.sh [dictcode].
+# If dictcode is not specified, web display code of all the dictionaries are updated.
+if [ -z "$1" ]
+  then
+	dicts=(BUR INM MWE PWG SKD STC VCP ACC AE AP90 AP BEN BHS BOP BOR CAE CCS GRA GST IEG KRM MCI MD MW72 MW PD PE PGN PUI PW SCH SHS SNP VEI WIL YAT)
+  else
+    dicts=(${1^^}) # Uppercase
+fi
+
+echo "Generating the web display code for the dictionaries."
 for dict in ${dicts[*]}
 do
 	python generate.py "$dict" inventory.txt  makotemplates ../../"$dict"Scan/2020/web
