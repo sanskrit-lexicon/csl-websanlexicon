2f4ae47dc57ee2a673ea1eef7ab95272877fd89f
funderburkjim <funderburkjim@gmail.com>
2020-11-04 16:12:59 -0500

    Minor changes so files will be same as those in csl-apidev

--- v02/makotemplates/web/webtc/getword.php
+++ v02/makotemplates/web/webtc/getword.php
@@ -1,15 +1,14 @@
 <?php
 // Exclude WARNING messages also, to solve Peter Scharf Mac version.
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING );
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 ?>
 <?php
 //getword.php
-
 if (isset($_GET['callback'])) {
  header('content-type: application/json; charset=utf-8');
 }
 header("Access-Control-Allow-Origin: *");
-require_once('getwordClass.php');
+require_once("getwordClass.php");
 function getwordCall() {
   $temp = new GetwordClass();
   $table1 = $temp->table1;
@@ -21,4 +20,4 @@ function getwordCall() {
   }
  }
  getwordCall();
-?> 
+?>
