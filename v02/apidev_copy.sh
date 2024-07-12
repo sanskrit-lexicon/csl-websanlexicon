b2d09d69cca7ae8d1301028f8c44de35740d26f0
funderburkjim <funderburkjim@gmail.com>
2024-07-12 12:46:28 -0400

    revise apidev_copy.sh -- coordination between csl-websanlexicon and csl-apidev repositories

--- v02/apidev_copy.sh
+++ v02/apidev_copy.sh
@@ -1,5 +1,7 @@
 # cp makotemplates/web/webtc/dal.php  ../../csl-apidev/ # normally no change to make
+echo "copying basicadjust.php, basicdisplay.php and getword_data.php csl-websanlexicon to csl-apidev
 cp /c/xampp/htdocs/cologne/csl-websanlexicon/v02/makotemplates/web/webtc/basicadjust.php /c/xampp/htdocs/cologne/csl-apidev/
 cp /c/xampp/htdocs/cologne/csl-websanlexicon/v02/makotemplates/web/webtc/basicdisplay.php /c/xampp/htdocs/cologne/csl-apidev/
 
+cp /c/xampp/htdocs/cologne/csl-websanlexicon/v02/makotemplates/web/webtc/getword_data.php /c/xampp/htdocs/cologne/csl-apidev/
 
