c8f473b699cb1b2c373c4e759ed78c40ba1a1a80
funderburkjim <funderburkjim@gmail.com>
2024-07-26 22:02:43 -0400

    Correct typo in apidev_copy.sh

--- v02/apidev_copy.sh
+++ v02/apidev_copy.sh
@@ -1,5 +1,5 @@
 # cp makotemplates/web/webtc/dal.php  ../../csl-apidev/ # normally no change to make
-echo "copying basicadjust.php, basicdisplay.php and getword_data.php csl-websanlexicon to csl-apidev
+echo "copying basicadjust.php, basicdisplay.php and getword_data.php csl-websanlexicon to csl-apidev"
 cp /c/xampp/htdocs/cologne/csl-websanlexicon/v02/makotemplates/web/webtc/basicadjust.php /c/xampp/htdocs/cologne/csl-apidev/
 cp /c/xampp/htdocs/cologne/csl-websanlexicon/v02/makotemplates/web/webtc/basicdisplay.php /c/xampp/htdocs/cologne/csl-apidev/
 
